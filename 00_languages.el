0f812dbc8dd29d189ba19770124c73d283849500
Dave Benvenuti <davebenvenuti@gmail.com>
2026-01-06 18:45:47 -0800

    Add markdown

--- 00_languages.el
+++ 00_languages.el
@@ -18,3 +18,10 @@
 
 (use-package nix-mode
   :mode "\\.nix\\'")
+
+(use-package markdown-mode
+  :ensure t
+  :mode ("README\\.md\\'" . gfm-mode)
+  :init (setq markdown-command "multimarkdown")
+  :bind (:map markdown-mode-map
+         ("C-c C-e" . markdown-do)))
